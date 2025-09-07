7e929a89d63991ad2b96f0976faee3a49fefcca4
Jiri Velek <jiri.velek@yahoo.com>
2025-09-07 03:13:35 +0200

    fix(docs): add correct psalm and phpstan return types for DenormalizerInterface

--- src/Symfony/Component/Serializer/Normalizer/DenormalizerInterface.php
+++ src/Symfony/Component/Serializer/Normalizer/DenormalizerInterface.php
@@ -29,10 +29,15 @@ interface DenormalizerInterface
     /**
      * Denormalizes data back into an object of the given class.
      *
-     * @param mixed       $data    Data to restore
-     * @param string      $type    The expected class to instantiate
-     * @param string|null $format  Format the given data was extracted from
-     * @param array       $context Options available to the denormalizer
+     * @template TObject of object
+     * @param mixed                             $data    Data to restore
+     * @param class-string<TObject>|string|null $type    The expected class to instantiate
+     * @param string|null                       $format  Format the given data was extracted from
+     * @param array                             $context Options available to the denormalizer
+     *
+     * @phpstan-return ($type is class-string<TObject> ? TObject : mixed)
+     *
+     * @psalm-return ($type is class-string<TObject> ? TObject : mixed)
      *
      * @throws BadMethodCallException   Occurs when the normalizer is not called in an expected context
      * @throws InvalidArgumentException Occurs when the arguments are not coherent or not supported
@@ -70,3 +75,4 @@ interface DenormalizerInterface
      */
     public function getSupportedTypes(?string $format): array;
 }
+
